b561aa393ece80ebaaabe0d80f23d2d49aa39ae0
hooperwf1 <hooperwf1@s.dcsdk12.org>
2021-02-20 14:37:54 -0700

    Basic linked list

--- main.c
+++ main.c
@@ -5,10 +5,12 @@
 #include "logging.h"
 #include "config.h"
 #include "communication.h"
+#include "linkedlist.h"
 
 int main(){
 	atexit(log_close);
 
+	/*
 	struct fig_ConfigData config = {0};
 	fig_readConfig("example_config.conf", &config);
 	log_editConfig(config.useFile, config.logDirectory);
@@ -25,5 +27,27 @@ int main(){
 	com_acceptClients(&sockAddr, &config);
 
 	close(sock);
+	*/
+
+	int *p;
+	int var = 10;
+	p = &var;
+	struct link_List list = {0};
+	link_add(&list, p);
+	*p += 10;
+	link_add(&list, p);
+	*p += 10;
+	link_add(&list, p);
+
+	struct link_Node *node = link_getNode(&list, 1);
+	if(!node){
+		printf("DEAD\n");
+		return -1;
+	}
+	
+	printf("M %p\n", list.head);
+	printf("%p\n", node);
+	printf("%d 1\n", *((int *)node->data));
+
 	return 0;
 }
