1b775ae2700b7e7bd6bfb77b6de13edabc4b6797
hooperwf1 <hooperwf1@s.dcsdk12.org>
2021-03-15 21:35:23 -0600

    init function

--- src/config.c
+++ src/config.c
@@ -8,6 +8,15 @@
 //Used to convert string option to an integer
 const char *options[] = {"port", "log", "enablelogging", "numthreads", "numclients"};
 
+// Struct to store all config data
+struct fig_ConfigData fig_Configuration;
+
+int init_config(char *dir){
+    fig_readConfig(dir);
+
+    return 1;
+}
+
 void fig_lowerString(char *str){
 	for(int i = 0; i < (int) strlen(str); i++){
 		str[i] = tolower(str[i]);
@@ -45,7 +54,7 @@ int fig_splitWords(char *line, char words[10][MAX_STRLEN]){
 	return word;
 }
 
-void fig_parseLine(char *line, struct fig_ConfigData* data){
+void fig_parseLine(char *line){
 	char words[10][MAX_STRLEN];		
 	int numWords = fig_splitWords(line, words);
 
@@ -66,51 +75,51 @@ void fig_parseLine(char *line, struct fig_ConfigData* data){
 		case 0:
 			//port
 			errno = 0;
-			data->port = strtol(words[1], NULL, 10);
+			fig_Configuration.port = strtol(words[1], NULL, 10);
 			if(errno != 0){
 				log_logError("Error converting string to int: port", WARNING);
-				data->port = 0;
+				fig_Configuration.port = 0;
 			}
 			break;
 
 		case 1:
 			//log
-			strncpy(data->logDirectory, words[1], ARRAY_SIZE(data->logDirectory));
+			strncpy(fig_Configuration.logDirectory, words[1], ARRAY_SIZE(fig_Configuration.logDirectory));
 			break;
 
 		case 2:
 			//enable logging
 			fig_lowerString(words[1]);
 			if(!strncmp(words[1], "true", MAX_STRLEN)){
-				data->useFile = 1;
+				fig_Configuration.useFile = 1;
 			} else {
-				data->useFile = 0;
+				fig_Configuration.useFile = 0;
 			}
 			break;
 
 		case 3:
 			//num threads
 			errno = 0;
-			data->threads = strtol(words[1], NULL, 10);
+			fig_Configuration.threads = strtol(words[1], NULL, 10);
 			if(errno != 0){
 				log_logError("Error converting string to int: threads", WARNING);
-				data->threads = 0;
+				fig_Configuration.threads = 0;
 			}
 			break;
 
 		case 4:
 			//max clients
 			errno = 0;
-			data->clients = strtol(words[1], NULL, 10);
+			fig_Configuration.clients = strtol(words[1], NULL, 10);
 			if(errno != 0){
 				log_logError("Error converting string to int: clients", WARNING);
-				data->clients = 0;
+				fig_Configuration.clients = 0;
 			}
 			break;
 	}
 }
 
-int fig_readConfig(char *path, struct fig_ConfigData* data){
+int fig_readConfig(char *path){
 	FILE* file;
 
 	file = fopen(path, "r");
@@ -122,7 +131,7 @@ int fig_readConfig(char *path, struct fig_ConfigData* data){
 	//read through every line
 	char buff[BUFSIZ];
 	while(fgets(buff, ARRAY_SIZE(buff), file)){
-		fig_parseLine(buff, data);
+		fig_parseLine(buff);
 	}
 
 	fclose(file);
