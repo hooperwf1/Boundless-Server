c5028574fcf6b0151a4d3e8d0c503a04b49b9250
hooperwf1 <hooperwf1@s.dcsdk12.org>
2021-07-10 16:07:18 -0600

    Server now quits if any inits fail, and gives a quitting message

--- src/roundtable.c
+++ src/roundtable.c
@@ -9,16 +9,27 @@
 #include "chat.h"
 #include "commands.h"
 
-int main(){
-    atexit(log_close);
-    atexit(com_close);
-    atexit(chat_close);
+void cleanUpServer(){
+	log_logMessage("Server is now quitting.", INFO);
 
-    init_config("example_config.conf"); /* config.h */
-    init_logging(); /* logging.h */
-    init_server(); /* communication.h */
-    init_commands(); /* commands.h */
-    init_chat(); /* chat.h */
+	log_close();
+	com_close();
+	chat_close();
+}
+
+int main(){
+    atexit(cleanUpServer);
+
+    if(init_config("example_config.conf") == -1) /* config.h */
+		return -1;
+    if(init_logging() == -1) /* logging.h */
+		return -1;
+    if(init_server() == -1) /* communication.h */
+		return -1;
+    if(init_commands() == -1) /* commands.h */
+		return -1;
+    if(init_chat() == -1) /* chat.h */
+		return -1;
 
     com_acceptClients();
 
